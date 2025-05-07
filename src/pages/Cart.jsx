b3fc1309ff9ab61d86537c5f6b69932964a77567
Nitin-0017 <raonitinkumar06@gmail.com>
2025-05-08 01:21:53 +0530

    folder structure

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,10 @@
+function Cart() {
+    return (
+      <div>
+        <h2>Your Cart</h2>
+        <p>Your cart is empty.</p>
+      </div>
+    );
+  }
+  
+  export default Cart;
---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,10 @@
+function ProductDetail() {
+    return (
+      <div>
+        <h2>Product Detail</h2>
+        <p>You're viewing details for product</p>
+      </div>
+    );
+  }
+  
+  export default ProductDetail;
