3c91893ec7c4e178616a00aef51a118d1264eb0e
Nitin-0017 <raonitinkumar06@gmail.com>
2025-09-16 22:53:37 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate,Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 
